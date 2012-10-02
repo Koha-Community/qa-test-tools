1446e80007e9a4b967e01288bc6ea8be1b47139a
Mason James <mtj@kohaaloha.com>
2012-10-03 09:46:38 +1300

    fixed perlcritic test

--- QohA/File/Perl.pm
+++ QohA/File/Perl.pm
@@ -61,20 +61,20 @@ sub check_critic {
         qx|rm $conf| if ( -e $conf ) ;
     }
 
-    # If the file does not exist anymore, we return 0
+    # If the tested file is deleted in the commit,  we return 0
     return 0 unless -e $self->path;
 
-    # If first pass returns 0 then the file did not exist
-    # And we have to pass Perl::Critic instead of Test::Perl::Critic::Progressive
-    if ( $self->report->tasks->{critic}
-            and $self->report->tasks->{critic}[0] == 0 ) {
+    # If the tested file is newly created int the commit, (ie: nothing existing to compare too),  we call Perl::Critic, not Test::Perl::Critic::Progressive
+     unless (-e $self->path)  {
         my $critic = Perl::Critic->new();
         # Serialize the violations to strings
         my @violations = map {
             my $v = $_; chomp $v; "$v";
         } $critic->critique($self->path);
         return \@violations;
-    }
+    } 
+
+#    else....
 
     # Check with Test::Perl::Critic::Progressive
     my $cmd = qq{
