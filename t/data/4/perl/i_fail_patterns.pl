a65e4d8359f7793748a14d170751cb140e5d6fc8
Mason James <mtj@kohaaloha.com>
2013-06-10 12:11:11 +1200

    fixing compile typo in t/data/4/perl/i_fail_patterns.pl

--- t/data/4/perl/i_fail_patterns.pl
+++ t/data/4/perl/i_fail_patterns.pl
@@ -1,9 +1,10 @@
 use Modern::Perl;
-
 use Data::Dumper::Dumper;
+
+# note: this file contains deliberate trailing spaces and tabs
 
 my $line_with_widespace = "bar";     
 
-	my $line_with_tab_character = "berk !"
+	my $line_with_tab_character = "berk !";
 
 warn Data::Dumper::Dumper $line_with_tab_character; # we don't want Data::Dumper::Dumper
